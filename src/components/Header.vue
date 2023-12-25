dcadc59a41d7202cdaea995c3a9e2083ab3fd7e8
alnasimul <alnasimul@gmail.com>
2023-12-25 23:17:09 +0600

    building tiny project using vuejs

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task"  color="green"/>
+        <!-- <Button text="Update Task"  color="orange"/>
+        <Button text="Delete Task"  color="red"/> -->
+    </header>
+</template>
+<script>
+
+import Button from "./Btn"
+    export default {
+        name: "Header",
+        props: {
+            title: String,
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
